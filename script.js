250222fbff516761a3ec9c0e2bdfe9291d494805
Kseniya <kukinaksu@yandex.ru>
2023-07-07 08:52:47 +0300

    Add homework1

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const menu = document.getElementById('nav__menu');
 
 document.addEventListener("click", function(e) {
     if (e.target.id != 'menu__btn' && e.target.id != 'nav__menu') {
-      menu.style.transform = 'translateX(150%)';
+        menu.style.transform = 'translateX(150%)';
     } else if (e.target.id == 'menu__btn') {
-      menu.style.transform = (menu.style.transform != 'translateX(0)') ? 'translateX(0)' : 'translateX(150%)';
+        menu.style.transform = (menu.style.transform != 'translateX(0)') ? 'translateX(0)' : 'translateX(150%)';
     }
-  });
+});
